13eaa7ed428f7716d818b17cd2a67cc12bfc9bf2
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-13 08:33:44 +0700

    Remove the header content from main

--- src/Main.vue
+++ src/Main.vue
@@ -3,14 +3,6 @@
 </script>
 
 <template>
-  Features
-  Company
-  Careers
-  About
-
-  Login
-  Register
-
   Make remote work
 
   Get your team in sync, no matter your location. Streamline processes, 
