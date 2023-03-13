275871e4f7deed84220a1a406162535208199ed6
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-13 07:45:11 +0700

    Store the content temporarily

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
+  Features
+  Company
+  Careers
+  About
 
+  Login
+  Register
+
+  Make remote work
+
+  Get your team in sync, no matter your location. Streamline processes, 
+  create team rituals, and watch productivity soar.
+
+  Learn more
 </template>
 
 <style scoped>
