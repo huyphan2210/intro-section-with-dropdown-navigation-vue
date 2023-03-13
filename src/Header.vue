a95c3b35d5fd7419c0507c977862ba4c8e0e8947
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-13 08:34:13 +0700

    Create the structure + basic styling

--- src/Header.vue
+++ src/Header.vue
@@ -3,21 +3,76 @@
 </script>
 
 <template>
-  Features
-  Company
-  Careers
-  About
-
-  Login
-  Register
-
-  Make remote work
-
-  Get your team in sync, no matter your location. Streamline processes, 
-  create team rituals, and watch productivity soar.
-
-  Learn more
+    <img id="logo" src="./assets/images/logo.svg" alt="logo">
+    <div class="headerSection">
+        <div class="navigations">
+          <nav>
+            <a class="dropdown">
+                Features
+                <img src="./assets/images/icon-arrow-down.svg">
+            </a>
+            <a class="dropdown">
+                Company
+                <img src="./assets/images/icon-arrow-down.svg">
+            </a>
+            <a>Careers</a>
+            <a>About</a>
+          </nav>
+        </div>
+        <div class="auth">
+            <button type="button">Login</button>
+            <button type="button">Register</button>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+#logo {
+    padding: 1rem;
+}
+.headerSection {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+    .navigations a {
+        position: relative;
+        margin-left: 1rem;
+        margin-right: 1rem;
+        color: var(--medium-gray);
+        font-weight: 700;
+    }
+        .navigations a:hover {
+            cursor: pointer;
+            color: var(--almost-black);
+        }
+    .navigations a img {
+        position: absolute;
+        top: 50%;
+        transform: translate(0, -50%);
+    }
+    
+        .navigations a:hover img {
+            filter: brightness(0.02);
+        }
+
+    .auth button {
+        padding: 1rem 2rem;
+        font-family: Epilogue, sans-serif;
+        border-radius: 1rem;
+        background-color: transparent;
+        color: var(--medium-gray);
+        font-weight: 700;
+        border-color: transparent;
+    }
+        .auth button:hover {
+            color: var(--almost-black);
+            cursor: pointer;
+        }
+    .auth button:last-child {
+        border-color: var(--medium-gray);
+    }
+        .auth button:last-child:hover {
+            border-color: var(--almost-black);
+        }
 </style>
